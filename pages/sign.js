2ae157846015f6af0a30a7b3ed20a7bef1c5eb0c
Lavishq <v.whitehearted@simplelogin.co>
2022-12-13 22:48:17 +0530

    working signature feature added

--- pages/sign.js
+++ pages/sign.js
@@ -1,12 +1,52 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
+import useHashMessage from "../hooks/useHash";
 import styles from "../styles/App.module.css";
 import Navbar from "./Navbar";
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
-  
+  const [user, setUser] = useState("");
+
+  const signMessage = async (account) => {
+    if (ethereum) {
+      // connect wallet
+      connectHandler();
+    }
+    if (!userInput) {
+      // no input
+      console.log("Enter message to sign");
+    }
+    if (account && userInput) {
+      // wallet connected and input
+      const hash = useHashMessage(userInput);
+      const signature = await ethereum.request({
+        method: "personal_sign",
+        params: [user.toString(), hash],
+      });
+      console.log(signature);
+      return signature;
+    }
+  };
+
+  const connectHandler = async () => {
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setUser(accounts[0]);
+        console.log(accounts[0]);
+      } catch (err) {
+        console.error(err);
+        console.log("There was a problem connecting to MetaMask");
+      }
+    } else {
+      alert("Install MetaMask");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -31,6 +71,7 @@ export default function Home() {
             setUserInput(e.target.value);
           }}
         />
+        <button onClick={signMessage}>sign message</button>
       </div>
 
       <footer className={styles.footer}>
